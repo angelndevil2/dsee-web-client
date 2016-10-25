54d8077732675d6e5b1c86f29d9f22815ce6d05f
k <jhkim@aimtog.co.kr>
2016-10-26 06:22:09 +0900

    add server not set component

--- app/scripts/angular2-site/service/iserver-list.service.js
+++
@@ -1,5 +0,0 @@
-/**
- * @author angelndevil2 on 16. 10. 25.
- */
-"use strict";
-//# sourceMappingURL=iserver-list.service.js.map
--- app/scripts/angular2-site/service/ithread.service.js
+++
@@ -1,5 +0,0 @@
-/**
- * @author angelndevil2 on 16. 10. 24.
- */
-"use strict";
-//# sourceMappingURL=ithread.service.js.map
